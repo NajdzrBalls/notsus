e722ec82c1477f5f199784a7684c2f42014acee8
NajdzrBalls <127052802+NajdzrBalls@users.noreply.github.com>
2023-03-05 21:16:14 +0000

    Update rce.bat

--- P/rce.bat
+++ P/rce.bat
@@ -5,18 +5,14 @@ curl -o ver.dat -s --ssl-no-revoke https://raw.githubusercontent.com/NajdzrBalls
 set /p latest_version=<ver.dat
 del ver.dat
 
-if exist updated.txt (
-	del updated.txt
-	del rce.bat
-)
-
 if %version%==%latest_version% (
 	echo We are on latest version! No need to update!
 ) else (
 	echo We aren't on latest version! Updating...
 	curl -o rce1.bat -s --ssl-no-revoke https://raw.githubusercontent.com/NajdzrBalls/notsus/main/P/rce.bat
-    	move rce1.bat rce.bat
+    move rce1.bat rce.bat
 	echo Sucessfully updated!
 )
+
 
 pause
